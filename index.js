9aa0be443faa85c765ac2c0e40a559fe1eecb364
MikiWojak <mikizarowa8@gmail.com>
2023-07-04 17:03:00 +0200

    ul

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-ReactDOM.render(<p>Hi, my name is Bob</p>, document.querySelector("#root"))
+ReactDOM.render(
+    <ul>
+        <li>Bucket helmet</li>
+        <li>Houndskull</li>
+        <li>Armet</li>
+    </ul>,
+    document.querySelector("#root")
+);
