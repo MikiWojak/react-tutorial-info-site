d574886bcc2a7c66bb91b9a839cd1bda5db2c006
MikiWojak <mikizarowa8@gmail.com>
2023-07-07 17:17:20 +0200

    Structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 
 const page = (
   <div>
-    <h1>Hello there</h1>
+    <img src="./react-logo.png" alt="react-logo" width="40px"/>
+
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
   </div>
 );
 
