4cc466ef7fc6a517fc9e18a38e46fdcc45f72de6
MikiWojak <mikizarowa8@gmail.com>
2023-07-07 17:05:28 +0200

    Remove unnecessary components

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 const page = (
   <div>
-    <h1>My website in React</h1>
-
-    <h3>Why React?</h3>
-    <ul>
-      <li>Composable</li>
-      <li>Declarative</li>
-      <li>Hireable</li>
-      <li>Maintained</li>
-    </ul>
+    <h1>Hello there</h1>
   </div>
 );
 
-// document.getElementById('root').append(JSON.stringify(page))
-
 ReactDOM.render(
   page,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
