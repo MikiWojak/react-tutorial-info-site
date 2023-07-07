53a9f1c09ce8e290a5f6a0def76048a61095e446
MikiWojak <mikizarowa8@gmail.com>
2023-07-07 17:35:09 +0200

    Component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const page = (
-  <div>
-    <img src="./react-logo.png" alt="react-logo" width="40px"/>
-
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
+function TemporaryName() {
+  return (
+    <div>
+      <img src="./react-logo.png" alt="react-logo" width="40px"/>
+  
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  page,
+  <TemporaryName />,
   document.getElementById("root")
 );
