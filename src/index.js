d53d0e8fb704527cce50e46652136c0dd4ef7f3a
MikiWojak <mikizarowa8@gmail.com>
2023-07-07 19:02:44 +0200

    App - components (one file)

--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" alt="react-logo" width="40px" />
+      </nav>
+    </header>
+  )
+}
+
+const Footer = () => {
+  return (
+    <footer>
+      <small>
+        &copy; 2023 MikiWojak development. All rights reserved.
+      </small>
+    </footer>
+  )
+}
+
+const MainContent = () => {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="./react-logo.png" alt="react-logo" width="40px" />
-        </nav>
-      </header>
-
       <h1>Reasons I'm excited to learn React</h1>
       <ol>
         <li>Something refreshing</li>
         <li>Hireable skill</li>
         <li>Broad horizons</li>
       </ol>
+    </div>
+  )
+}
 
-      <footer>
-        <small>
-          &copy; 2023 MikiWojak development. All rights reserved.
-        </small>
-      </footer>
+const App = () => {
+  return (
+    <div>
+      <Header />
+      
+      <MainContent />
+
+      <Footer />
     </div>
   )
 }
