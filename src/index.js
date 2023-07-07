a84cebbf35a143ea63dcea0b1abf8d251726a41b
MikiWojak <mikizarowa8@gmail.com>
2023-07-07 17:50:17 +0200

    Components challenge - WIP

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function TemporaryName() {
+function App() {
   return (
     <div>
-      <img src="./react-logo.png" alt="react-logo" width="40px"/>
-  
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      <header>
+        <nav>
+          <img src="./react-logo.png" alt="react-logo" width="40px" />
+        </nav>
+      </header>
+
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>Something refreshing</li>
+        <li>Hireable skill</li>
+        <li>Broad horizons</li>
+      </ol>
+
+      <footer>
+        <small>
+          &copy; 2023 MikiWojak development. All rights reserved.
+        </small>
+      </footer>
     </div>
-  );
+  )
 }
 
 ReactDOM.render(
-  <TemporaryName />,
+  <App />,
   document.getElementById("root")
 );
