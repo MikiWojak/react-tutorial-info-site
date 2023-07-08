3920540cd9c75cf6088f1658d74e57acca951d39
Mikołaj <53621297+MikiWojak@users.noreply.github.com>
2023-07-08 09:16:06 +0200

    Basic React (webpack) (#2)
    - Create app using Create React App
    - Basics of React

---
+++ src/components/content/MainContent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MainContent = () => {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>Something refreshing</li>
+                <li>Hireable skill</li>
+                <li>Broad horizons</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MainContent
