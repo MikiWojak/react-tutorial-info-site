9fb4daa761babae820ed0ef6f291f6d24a0b8a9d
MikiWojak <mikizarowa8@gmail.com>
2023-07-09 16:00:09 +0200

    Style Nav, basic Main

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import reactLogo from '../../assets/images/react-icon-small.png'
+
 const Navbar = () => {
     return (
-        <nav>
-            <h1>Navbar goes here</h1>
+        <nav className="nav">
+            <div className="nav-logo">
+                <img src={reactLogo} alt="react-logo" className="nav-logo-img" />
+                <h2 className="nav-logo-text">ReactFacts</h2>
+            </div>
+
+            <h3 className="nav-item">React Course - Project 1</h3>
         </nav>
     )
 }
