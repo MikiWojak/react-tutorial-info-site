4f463877f5a8456f793d7f98fbf7c74cab9dc7ab
MikiWojak <mikizarowa8@gmail.com>
2023-07-09 16:16:56 +0200

    Style Main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Main = () => {
     return (
         <main className="main">
-            <h1 className="main-header">Fun facts about React</h1>
-            <ul className="main-list">
+            <h1 className="main--header">Fun facts about React</h1>
+            <ul className="main--list">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
