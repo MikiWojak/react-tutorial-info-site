bfd545580c61ffb8cd997c9297dc553ea3b1f883
MikiWojak <mikizarowa8@gmail.com>
2023-07-09 16:09:21 +0200

    Navbar - fixes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,12 +5,12 @@ import reactLogo from '../../assets/images/react-icon-small.png'
 const Navbar = () => {
     return (
         <nav className="nav">
-            <div className="nav-logo">
-                <img src={reactLogo} alt="react-logo" className="nav-logo-img" />
-                <h2 className="nav-logo-text">ReactFacts</h2>
+            <div className="nav--logo">
+                <img src={reactLogo} alt="react-logo" className="nav--logo_img" />
+                <h3 className="nav--logo_text">ReactFacts</h3>
             </div>
 
-            <h3 className="nav-item">React Course - Project 1</h3>
+            <h4 className="nav--item">React Course - Project 1</h4>
         </nav>
     )
 }
