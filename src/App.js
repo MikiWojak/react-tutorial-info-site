771f3d729d349c70e116847c29436d9ac4b1080d
MikiWojak <mikizarowa8@gmail.com>
2023-07-24 10:49:56 +0200

    Change mode - hardcoded

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Main from './components/main/Main'
 import Navbar from './components/layout/Navbar'
 
 const App = () => {
+    const [darkMode] = useState(false)
+
     return (
         <div className="container">
-            <Navbar />
+            <Navbar darkMode={darkMode}/>
 
-            <Main />
+            <Main darkMode={darkMode}/>
         </div>
     )
 }
