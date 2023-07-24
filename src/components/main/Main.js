90bdb0e57cf1616a9422a0d435b69a011b05b9a8
MikiWojak <mikizarowa8@gmail.com>
2023-07-24 10:39:34 +0200

    Implement dark mode - prepare

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Main = () => {
+const Main = (props) => {
     return (
-        <main className="main">
-            <h1 className="main--header">Fun facts about React</h1>
-            <ul className="main--list">
+        <main className={props.darkMode ? "dark" : ""}>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
